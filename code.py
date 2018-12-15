65a3f93112147fa19de1729b0f38c8762724c1fd
Germanova Polina <buchurella@yandex.ru>
2018-12-15 22:45:53 +0300

    two new functions added

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtWidgets import QInputDialog, QMessageBox
 from PyQt5.QtCore import QCoreApplication
+import string
 
 
 class MyWidget(QMainWindow):
@@ -10,12 +11,14 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('filedialog.ui',self)
         
-        self.buttons = [self.trns]
+        self.buttons = [self.trns, self.cphr, self.chw]
         self.fin, self.out = '', ''
         self.end.hide()
         
         self.btn.clicked.connect(self.getFiles)
         self.trns.clicked.connect(self.transliteration)
+        self.cphr.clicked.connect(self.cipher)
+        self.chw.clicked.connect(self.change_word)
         
     def getFiles(self):
         i, okBtnPressed = QInputDialog.getText(
@@ -28,13 +31,14 @@ class MyWidget(QMainWindow):
                 self.fout = open(i.split()[1].strip(), 'w')
                 for button in self.buttons:
                     button.setEnabled(True)
+                self.end.hide()
             except AssertionError as a:
                 msg = QMessageBox.critical(self, "Предупреждение",
-                                           "Проверьте, что вы ввели два файла!",
+                                           "Введите два файла!",
                                            QMessageBox.Close)
             except FileNotFoundError as f:
                 msg = QMessageBox.critical(self, "Предупреждение",
-                                           "Не найден такой файл!  ",
+                                           "Не найден файл ввода!  ",
                                            QMessageBox.Close)
             
                 
@@ -61,6 +65,63 @@ class MyWidget(QMainWindow):
         self.fout.write(word)
         self.fout.close()
         self.end.show()
+        for button in self.buttons:
+            button.setEnabled(False)  
+            
+    def cipher(self):
+        shift, okBtnPressed = QInputDialog.getInt(
+            self, "Смещение", "Выберите сдвиг", 0, 0, 25, 1)
+        if okBtnPressed:        
+            msg = self.fin.read()
+            message = ''
+            for i in range(len(msg)):
+                if 1071 < ord(msg[i].lower()) < 1104:
+                    if msg[i] == msg[i].lower():
+                        message += chr((ord(msg[i]) - 1072 + shift) % 32 + 1072)
+                    else:
+                        message += chr((ord(msg[i].lower()) - 1072 + shift) % 32 + 1072).upper()
+                if 97 <= ord(msg[i].lower()) <= 122:
+                    if msg[i] == msg[i].lower():
+                        message += chr((ord(msg[i]) - 97 + shift) % 26 + 97)
+                    else:
+                        message += chr((ord(msg[i].lower()) - 97 + shift) % 26 + 97).upper()            
+                else:
+                    message += msg[i]
+            self.fout.write(message)
+            self.fout.close()
+            self.end.show()
+            for button in self.buttons:
+                button.setEnabled(False)
+    
+    def change_word(self):
+        try:
+            i, okBtnPressed = QInputDialog.getText(
+                self, "Cлова", "Введите два слова через пробел:\nкоторое хотите заменить и на которое хотите заменить") 
+            if okBtnPressed:
+                assert len(i.split()) == 2
+                word1 = i.split()[0]
+                word2 = i.split()[1]
+                text = self.fin.read().split()
+                n_text = []
+                sample = ''
+                for word in text:
+                    sample = word
+                    if word.lower().strip(string.punctuation) == word1:
+                        sample = word2
+                        if word[0] == word[0].upper() or word[0] in string.punctuation:
+                            sample = sample[0].upper() + sample[1::]
+                        if word[-1] in string.punctuation:
+                            sample = sample + word[-1]
+                    n_text.append(sample)
+            self.fout.write(' '.join(n_text))
+            self.fout.close()
+            self.end.show()
+            for button in self.buttons:
+                button.setEnabled(False)              
+        except AssertionError as a:
+            msg = QMessageBox.critical(self, "Предупреждение",
+                                       "Введите два слова!",
+                                       QMessageBox.Close)        
         
 
 app = QApplication(sys.argv)
