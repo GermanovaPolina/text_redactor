415919f1ed3e54e58ec41f6772b8423afe7b24aa
Germanova Polina <buchurella@yandex.ru>
2018-12-16 18:37:36 +0300

    final version. still python

--- code.py
+++ code.py
@@ -9,9 +9,10 @@ import string
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('filedialog.ui',self)
+        uic.loadUi('filedialog.ui', self)
         
-        self.buttons = [self.trns, self.cphr, self.chw]
+        self.buttons = [self.trns, self.cphr, self.chw, self.keys, self.rev,
+                        self.stata]
         self.fin, self.out = '', ''
         self.end.hide()
         
@@ -19,11 +20,13 @@ class MyWidget(QMainWindow):
         self.trns.clicked.connect(self.transliteration)
         self.cphr.clicked.connect(self.cipher)
         self.chw.clicked.connect(self.change_word)
+        self.keys.clicked.connect(self.qwerty)
+        self.rev.clicked.connect(self.rtext)
+        self.stata.clicked.connect(self.statistics)
         
     def getFiles(self):
         i, okBtnPressed = QInputDialog.getText(
-            self, "Файлы", "Введите имена файлов ввода\nи вывода через пробел"
-        )
+            self, "Файлы", "Введите имена файлов ввода\nи вывода через пробел")
         if okBtnPressed:
             try:
                 assert len(i.split()) == 2
@@ -40,9 +43,7 @@ class MyWidget(QMainWindow):
                 msg = QMessageBox.critical(self, "Предупреждение",
                                            "Не найден файл ввода!  ",
                                            QMessageBox.Close)
-            
-                
-            
+                   
     def transliteration(self):
         alphabet = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e',
                     'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'i',
@@ -70,21 +71,24 @@ class MyWidget(QMainWindow):
             
     def cipher(self):
         shift, okBtnPressed = QInputDialog.getInt(
-            self, "Смещение", "Выберите сдвиг", 0, 0, 25, 1)
+            self, "Смещение", "Выберите сдвиг", 0, -25, 25, 1)
         if okBtnPressed:        
             msg = self.fin.read()
             message = ''
             for i in range(len(msg)):
-                if 1071 < ord(msg[i].lower()) < 1104:
+                if 1072 <= ord(msg[i].lower()) <= 1103:
                     if msg[i] == msg[i].lower():
-                        message += chr((ord(msg[i]) - 1072 + shift) % 32 + 1072)
+                        message += chr((ord(msg[i]) - 1072 + shift) % 33 +
+                                       1072)
                     else:
-                        message += chr((ord(msg[i].lower()) - 1072 + shift) % 32 + 1072).upper()
-                if 97 <= ord(msg[i].lower()) <= 122:
+                        message += chr((ord(msg[i].lower()) - 1072 + shift) %
+                                       33 + 1072).upper()
+                elif 97 <= ord(msg[i].lower()) <= 122:
                     if msg[i] == msg[i].lower():
                         message += chr((ord(msg[i]) - 97 + shift) % 26 + 97)
                     else:
-                        message += chr((ord(msg[i].lower()) - 97 + shift) % 26 + 97).upper()            
+                        message += chr((ord(msg[i].lower()) - 97 + shift) %
+                                       26 + 97).upper()            
                 else:
                     message += msg[i]
             self.fout.write(message)
@@ -96,7 +100,9 @@ class MyWidget(QMainWindow):
     def change_word(self):
         try:
             i, okBtnPressed = QInputDialog.getText(
-                self, "Cлова", "Введите два слова через пробел:\nкоторое хотите заменить и на которое хотите заменить") 
+                self, "Cлова",
+                "Введите два слова через пробел:\nкоторое хотите \
+заменить и на которое хотите заменить") 
             if okBtnPressed:
                 assert len(i.split()) == 2
                 word1 = i.split()[0]
@@ -108,7 +114,8 @@ class MyWidget(QMainWindow):
                     sample = word
                     if word.lower().strip(string.punctuation) == word1:
                         sample = word2
-                        if word[0] == word[0].upper() or word[0] in string.punctuation:
+                        if (word[0] == word[0].upper() or
+                            word[0] in string.punctuation):
                             sample = sample[0].upper() + sample[1::]
                         if word[-1] in string.punctuation:
                             sample = sample + word[-1]
@@ -121,10 +128,98 @@ class MyWidget(QMainWindow):
         except AssertionError as a:
             msg = QMessageBox.critical(self, "Предупреждение",
                                        "Введите два слова!",
-                                       QMessageBox.Close)        
+                                       QMessageBox.Close)
+            
+    def qwerty(self):
+        ru_en = {'а': 'f', 'б': ',', 'в': 'd', 'г': 'u', 'д': 'l', 'е': 't',
+                    'ё': '`', 'ж': ';', 'з': 'p', 'и': 'b', 'й': 'q',
+                    'к': 'r', 'л': 'k', 'м': 'v', 'н': 'y', 'о': 'j', 'п': 'g',
+                    'р': 'h', 'с': 'c', 'т': 'n', 'у': 'e', 'ф': 'a',
+                    'х': '[', 'ц': 'w', 'ч': 'x', 'ш': 'i', 'щ': 'o',
+                    'ы': 's', 'э': '\'', 'ю': '.', 'я': 'z', 'ь': 'm',
+                    'ъ': ']', 'Э': '"', 'Ж': ':', 'Ъ': '}', 'Х': '{', 'Б': '<',
+                    'Ю': '>', '.': '/', ',': '?'}
+        en_ru = dict([(v, k) for k, v in ru_en.items()])
+        print(en_ru)
+        i, okBtnPressed = QInputDialog.getItem(
+            self, "Раскладка", "Из какой раскладки вы хотите перевод?",
+            ('русская', 'английская'), 1, False) 
+        if okBtnPressed:
+            text = self.fin.read()
+            n_text = ''
+            if i == 'русская':
+                for letter in text:
+                    if letter in ru_en:
+                        n_text += ru_en[letter]
+                    elif letter.lower() in ru_en:
+                        n_text += ru_en[letter.lower()].upper()
+                    else:
+                        n_text += letter
+            else:
+                for letter in text:
+                    if letter in en_ru:
+                        n_text += en_ru[letter]
+                    elif letter.lower() in en_ru:
+                        n_text += en_ru[letter.lower()].upper()
+                    else:
+                        n_text += letter
+            self.fout.write(n_text)
+            self.fout.close()
+            self.end.show()
+            for button in self.buttons:
+                button.setEnabled(False) 
+    
+    def rtext(self):
+        text = self.fin.read()
+        n_text = []
+        for word in text.split()[::-1]:
+            if word[0] == word[0].upper():
+                n_text.append(word[-1].upper() + word[-2::-1])
+            else:
+                n_text.append(word[::-1])
+        self.fout.write(' '.join(n_text))
+        self.fout.close()
+        self.end.show()
+        for button in self.buttons:
+            button.setEnabled(False)
+            
+    def statistics(self):
+        text = self.fin.read().split()
+        words = dict()
+        letters = dict()
+        for word in text:
+            word = word.lower().strip(string.punctuation)
+            words[word] = words.get(word, 0) + 1
+            for letter in word:
+                letters[letter] = letters.get(letter, 0) + 1
+        words = sorted([(v, k) for k, v in words.items()],
+                       key=lambda x: (-x[0], x[1]))
+        letters = sorted([(v, k) for k, v in letters.items()],
+                         key=lambda x: (-x[0], x[1]))
+        if len(words) < 10:
+            n_w = len(words) + 1
+        else:
+            n_w = 11
+        if len(letters) < 10:
+            n_l = len(letters) + 1
+        else:
+            n_l = 11
+        self.fout.write('{} самых использованных слов:\n'.format(n_w - 1))
+        for i in range(1, n_w):
+            self.fout.write('{}. {} - {} раз\n'.format(i, words[i-1][1],
+                                                          words[i-1][0]))
+        self.fout.write('{} самых использованных символов:\n'.format(n_l - 1))
+        for i in range(1, n_l):
+            self.fout.write('{}. {} - {} раз\n'.format(i, letters[i-1][1],
+                                                          letters[i-1][0]))
+        self.fout.close()
+        self.end.show()
+        for button in self.buttons:
+            button.setEnabled(False)
         
 
-app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
